647f0427198bee6fdd64f7ed394cf4d90bdd4479
Peterson Lima <peter.lima104dev@hotmail.com>
2023-06-20 17:25:36 -0300

    Insert class BubbleSort run ok

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        BubbleSort.bubbleSort(arr);
+        System.out.println("Array ordenado:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
